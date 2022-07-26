a78659e5191af2b4716460ed936b8988714abf64
jleeman22 <109816562+jleeman22@users.noreply.github.com>
2022-07-26 14:42:01 -0400

    Add files via upload

---
+++ My Jupyter Notebook.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __John Leeman__ <br>
+# IT Specialist, Information Resource Management
+
+# _I am interested in data science because I wish to expand my existing data anayltic knowledge to become a Data Scientist._
+
+# ### The following code will do a simple math example:
+
+# In[2]:
+
+
+(3 * 3)
+
+
+# ***
+# > 1. Here is a numbered list
+# > 1. Which contains <a id="section_ID">hyperlinks</a>
+# > 1. <code style="background:yellow;color:black">Highlighting</code> and 
+# > 1. ~~strikethrough~~
+# ***
+
+# In[ ]:
+
+
+
+
